52d7c21a185c4fa25cba00f1469ec604bb191965
KuznetsovMikhail <KuznetsovMikhail>
2021-01-18 14:08:04 +0300

    add whitespace between // and comment

--- src/main.cpp
+++ src/main.cpp
@@ -8,9 +8,9 @@
 int main() {
   std::string s1("(18 + 01 + 20.21) * (12.02 - 10 - 81) / 1");
   std::string s2 = infix2postfix(s1);
-  std::cout << s2 << std::endl;   //18 01 + 20.21 + 12.02 10 - 81 - * 1 /
+  std::cout << s2 << std::endl;   // 18 01 + 20.21 + 12.02 10 - 81 - * 1 /
   s1 = "1 + (1 + (1 + (1 + 1) + 1) + 1) + 1";
   s2 = infix2postfix(s1);
-  std::cout << s2 << std::endl;   //1 1 1 1 1 + + 1 + + 1 + + 1 +
+  std::cout << s2 << std::endl;   // 1 1 1 1 1 + + 1 + + 1 + + 1 +
   return 0;
 }
